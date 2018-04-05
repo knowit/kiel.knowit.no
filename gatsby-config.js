0340514bef7ab7e31f8c5f0a23601b2d62e0edd5
Christian Peter <christian150790@gmail.com>
2018-04-05 11:56:52 +0200

    Adds new config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Knowit Kiel 2018',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: '/kiel.knowit.no'
+  plugins: ['gatsby-plugin-react-helmet']
 };
