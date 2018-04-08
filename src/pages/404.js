1207fd798f3b7d35328007f029bfa6cb3faa1ec9
Christian Peter <christian150790@gmail.com>
2018-04-08 21:39:07 +0200

    Adds 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import BEMHelper from 'react-bem-helper'
+import Link from 'gatsby-link'
+import SadFace from '../components/Icons/SadFace';
+import ButtonGroup from '../components/ButtonGroup'
+
+const classes = new BEMHelper({
+  name: 'content',
+  prefix: 'c-',
+})
+
+const buttonClasses = new BEMHelper({
+  name: 'button',
+  prefix: 'c-',
+})
 
 const NotFoundPage = () => (
-  <div>
-    <h1>Ups</h1>
-    <p>Denne siden finnes ikke :(</p>
+  <div {...classes('', 'not-found')}>
+    <div {...classes('container')}>
+    <SadFace />
+    <p>Denne siden finnes ikke</p>
+    <h2>Men har du prøvd en av disse?</h2>
+      <ButtonGroup>
+        <Link {...buttonClasses('', 'white')} to="/">
+          Forside
+        </Link>
+        <Link {...buttonClasses('', 'white')} to="/speakers/">
+          Talere
+        </Link>
+        <Link {...buttonClasses('', 'white')} to="/schedule/">
+          Program
+        </Link>
+      </ButtonGroup>
+    </div>
   </div>
 )
 
