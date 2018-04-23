68650702240940b385daed4a24c23610d3c18152
Christian Peter <christian150790@gmail.com>
2018-04-23 14:57:15 +0200

    adds manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,25 @@ module.exports = {
     title: 'Knowit Kiel 2018',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "kiel.knowit.no",
+        short_name: "Kiel",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icon: "public/static/logo_kielferga.png", // This path is relative to the root of the site.
+        icons: [
+          {
+            src: "static/logo_kielferga192x192.png",
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
     `gatsby-plugin-offline`,
     'gatsby-plugin-react-helmet',
   ],
